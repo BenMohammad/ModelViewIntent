994a3c6afc11db657d0f9611804214a38787e13a
Ben Mohammad <benmohammad593@gmail.com>
2020-07-20 12:19:39 +0100

    TasksFilterType.kt, ScrollChildSwipeRefreshLayout.kt added tasks package...

---
+++ app/src/main/java/com/benmohammad/modelviewintent/data/tasks/ScrollChildSwipeRefreshLayout.kt
@@ -0,0 +1,23 @@
+package com.benmohammad.modelviewintent.data.tasks
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+
+class ScrollChildSwipeRefreshLayout: SwipeRefreshLayout {
+
+    private var scrollUpChild: View? = null
+
+    constructor(context: Context): super(context)
+
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
+
+    override fun canChildScrollUp(): Boolean {
+        return scrollUpChild?.canScrollVertically(-1) ?: super.canChildScrollUp()
+    }
+
+    fun setScrollUpChild(view: View) {
+        scrollUpChild = view
+    }
+}
---
+++ app/src/main/java/com/benmohammad/modelviewintent/data/tasks/TasksFilterType.kt
@@ -0,0 +1,8 @@
+package com.benmohammad.modelviewintent.data.tasks
+
+enum class TasksFilterType {
+
+    ALL_TASKS,
+    ACTIVE_TASKS,
+    COMPLETED_TASKS
+}
