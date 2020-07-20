0c968ce4dce3f868c12a4bd6da1584be6e2470b0
Ben Mohammad <benmohammad593@gmail.com>
2020-07-20 12:29:37 +0100

    TasksIntent.kt, TasksResult.kt added...

---
+++ app/src/main/java/com/benmohammad/modelviewintent/data/tasks/TasksIntent.kt
@@ -0,0 +1,14 @@
+package com.benmohammad.modelviewintent.data.tasks
+
+import com.benmohammad.modelviewintent.data.Task
+import com.benmohammad.modelviewintent.mvibase.MviIntent
+
+open class TasksIntent: MviIntent {
+    object InitialIntent: TasksIntent()
+
+    data class RefreshIntent(val forceUpdate: Boolean): TasksIntent()
+    data class ActivateTaskIntent(val task: Task): TasksIntent()
+    data class CompleteTaskIntent(val task: Task): TasksIntent()
+    object ClearCompletedTaskIntent: TasksIntent()
+    data class ChangeFilterIntent(val filterType: TasksFilterType): TasksIntent()
+}
---
+++ app/src/main/java/com/benmohammad/modelviewintent/data/tasks/TasksResult.kt
@@ -0,0 +1,34 @@
+package com.benmohammad.modelviewintent.data.tasks
+
+import com.benmohammad.modelviewintent.data.Task
+import com.benmohammad.modelviewintent.mvibase.MviResult
+
+open class TasksResult: MviResult {
+
+    sealed class LoadTaskResult: TasksResult() {
+        data class Success(val tasks: List<Task>, val filterType: TasksFilterType?): LoadTaskResult()
+        data class Failure(val error: Throwable): LoadTaskResult()
+        object InFlight: TasksResult()
+    }
+
+    sealed class ActivateTaskResult: TasksResult() {
+        data class Success(val tasks: List<Task>): ActivateTaskResult()
+        data class Failure(val error: Throwable): ActivateTaskResult()
+        object InFlight: ActivateTaskResult()
+        object HideUiNotification: ActivateTaskResult()
+    }
+
+    sealed class CompleteTaskResult: TasksResult() {
+        data class Success(val tasks: List<Task>): CompleteTaskResult()
+        data class Failure(val error: Throwable): CompleteTaskResult()
+        object InFlight: CompleteTaskResult()
+        object HideUiNotification: CompleteTaskResult()
+    }
+
+    sealed class ClearCompletedTasksResult: TasksResult() {
+        data class Success(val tasks: List<Task>): ClearCompletedTasksResult()
+        data class Failure(val error: Throwable): ClearCompletedTasksResult()
+        object InFlight: ClearCompletedTasksResult()
+        object HideUiNotification: ClearCompletedTasksResult()
+    }
+}
